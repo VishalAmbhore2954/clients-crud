2db5ea1fb61cff5f0c7fbf9cc6caf9b0b893a891
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2026-04-27 10:26:34 +0530

    Update GSTIN validation rules in StoreClientRequest

--- app/Http/Requests/ClientRequest/StoreClientRequest.php
+++ app/Http/Requests/ClientRequest/StoreClientRequest.php
@@ -27,7 +27,7 @@ class StoreClientRequest extends FormRequest
             'name'   => ['required','string','max:255','regex:/^[A-Za-z\s]+$/'],
             'email'  => 'required|email|unique:clients,email',
             'phone'  => 'required|string|min:10|max:15',
-            'gstin'  => 'nullable|alpha_num|size:15',
+            'gstin'  => ['nullable','size:15','regex:/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/'],
             'city'   => 'required|string',
             'status' => ['required', Rule::in(['Active', 'Inactive'])],
         ];
